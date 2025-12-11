5359b2b8373e22fd420be0082e3603a429ffc181
oleksandr_smiian <oleksandr_smiian@tchdm.live>
2025-12-11 09:12:32 +0200

    Updates

--- quiz/src/shared/lib/validation.ts
+++
@@ -1,5 +0,0 @@
-export function validateEmail(email: string): boolean {
-    const regexpEmail =
-        /^[A-Za-z0-9_]+([+.-][A-Za-z0-9_]+)*@([A-Za-z0-9]+[-]*[A-Za-z0-9]+)(\.[A-Za-z0-9]+){1,2}$/;
-    return regexpEmail.test(email);
-}
